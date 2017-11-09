8e1fbecafb45351781cd95b8348620851d66c0b9
colelabar <31549550+colelabar@users.noreply.github.com>
2017-11-09 16:22:24 -0500

    continued structuring the collapsable elements, almost working

--- speakers.js
+++ speakers.js
@@ -1,10 +1,11 @@
-var dropdown = document.getElementsByClassName('dropDown');
-
-dropdown.onclick = function show() {
-  var dropContent = document.getElementsByClassName('dropdownContent');
-  if (dropContent.css({'display' : 'none'})) {
-    dropContent.css({'display' : 'block'});
-  } else if (dropContent.css({'display' : 'block'})) {
-    dropContent.css({'display' : 'none'});
+var dropdown = document.getElementsByClassName('dropDown')[0];
+dropdown.addEventListener('click', function show() {
+  var dropContent = document.getElementsByClassName('dropdownContent')[0];
+  if (dropContent.classList == 'dropdownContent') {
+    dropContent.classList.add('none');
+    dropContent.classList.remove('dropdownContent');
+  } else if (dropContent.classList == 'none') {
+    dropContent.classList.add('dropdownContent');
+    dropContent.classList.remove('none');
   }
-}
+});
