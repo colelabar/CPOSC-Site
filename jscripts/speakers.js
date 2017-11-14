0b907bd1fb039cdd47cae7249952f6e3d1be9e6f
colelabar <31549550+colelabar@users.noreply.github.com>
2017-11-14 14:08:44 -0500

    added more scripts, modularized existing scripts to make them not as repetitive

--- jscripts/speakers.js
+++ jscripts/speakers.js
@@ -1,28 +1,4 @@
-function show() {
+function show(x) {
   var dropContent = document.getElementsByClassName('dropdownContent');
-      dropContent[0].classList.toggle('none');
+      dropContent[x].classList.toggle('none');
     }
-function show1() {
-  var dropContent = document.getElementsByClassName('dropdownContent');
-      dropContent[1].classList.toggle('none');
-}
-function show2() {
-  var dropContent = document.getElementsByClassName('dropdownContent');
-      dropContent[2].classList.toggle('none');
-  }
-function show3() {
-  var dropContent = document.getElementsByClassName('dropdownContent');
-      dropContent[3].classList.toggle('none');
-    }
-function show4() {
-  var dropContent = document.getElementsByClassName('dropdownContent');
-      dropContent[4].classList.toggle('none');
-}
-function show5() {
-  var dropContent = document.getElementsByClassName('dropdownContent');
-      dropContent[5].classList.toggle('none');
-  }
-function show6() {
-  var dropContent = document.getElementsByClassName('dropdownContent');
-      dropContent[6].classList.toggle('none');
-  }
